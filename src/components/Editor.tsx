1978fae149641adee0166c1af5b2c7f4e07547d0
Patryk Skolimowski <revkuu@gmail.com>
2026-01-17 12:20:15 +0100

    feat: add note hashing feature

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,11 +1,28 @@
-import { useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
+import LZString from "lz-string";
 
 export function Editor() {
   const [content, setContent] = useState<string>("");
+  const [hash, setHash] = useState<string>("");
+
+  useLayoutEffect(() => {
+    const windowHash = window.location.hash;
+    if (!windowHash) return;
+
+    setContent(LZString.decompressFromEncodedURIComponent(windowHash.slice(1)));
+  }, []);
 
   useEffect(() => {
     if (content.length == 0) return;
+
+    setHash(LZString.compressToEncodedURIComponent(content));
   }, [content]);
+
+  useEffect(() => {
+    if (!hash) return;
+
+    window.location.hash = hash;
+  }, [hash]);
 
   return (
     <div className="py-5 h-full">
